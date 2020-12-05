12170b108a60a2081f9c8bf9eae0934968a1920f
sofyanmoch <mchsfyan17@gmail.com>
2020-12-05 23:47:18 +0700

    found bug

--- src/food/food.controller.ts
+++ src/food/food.controller.ts
@@ -1,56 +1,71 @@
-import { Controller, Get, Res, HttpStatus, Param, NotAcceptableException, Post, Body, Query, Put, Delete, NotFoundException } from '@nestjs/common';
-import { FoodService } from './food.service'
-import {CreateFoodDTO} from './dto/create-food.dto'
-import {ValidateObjectId} from '../shared/pipes/validate-object-id.pipes'
+import {
+  Controller,
+  Get,
+  Res,
+  HttpStatus,
+  Param,
+  NotAcceptableException,
+  Post,
+  Body,
+  Query,
+  Put,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+import { FoodService } from './food.service';
+import { CreateFoodDTO } from './dto/create-food.dto';
+import { ValidateObjectId } from './shared/pipes/validate-object-id.pipes';
 
 @Controller('food')
 export class FoodController {
+  constructor(private foodService: FoodService) {}
 
-    constructor(private foodService: FoodService) { }
+  @Get('/')
+  async getFoods(@Res() res) {
+    const food = await this.foodService.getFoods();
+    return res.status(HttpStatus.OK).json(food);
+  }
 
-    @Get('/')
-    async getFood(@Res() res) {
-        const food = await this.foodService.getFood()
-        return res.status(HttpStatus.OK).json(food)
-    }
+  @Get('/:foodID')
+  async getFood(@Res() res, @Param('foodID', new ValidateObjectId()) foodID) {
+    const food = await this.foodService.getFood(foodID);
+    if (!food) throw new NotFoundException('Food tidak ada!');
+    return res.status(HttpStatus.OK).json(food);
+  }
 
-    @Get('/:foodID')
-    async getFood(@Res() res, @Param('foodID', new ValidateObjectId()) foodID) {
-        const food = await this.foodService.getFood(foodID);
-        if(!food) throw new NotFoundException('Food tidak ada!')
-        return res.status(HttpStatus.OK).json(food)
-    }
+  @Post('/add')
+  async addFood(@Res() res, @Body() createFoodDTO: CreateFoodDTO) {
+    const newFood = await this.foodService.addFood(createFoodDTO);
+    return res.status(HttpStatus.OK).json({
+      message: 'Add food success',
+      food: newFood,
+    });
+  }
 
-    @Post('/add')
-    async addFood(@Res() res, @Body() createFoodDTO: CreateFoodDTO) {
-        const newFood = await this.foodService.addFood(createFoodDTO)
-        return res.status(HttpStatus.OK).json({
-            message: 'Add food success',
-            food: newFood
-        })
-    }
+  @Put('/edit')
+  async editFood(
+    @Res() res,
+    @Query('foodID', new ValidateObjectId()) foodID,
+    @Body() createFoodDTO: CreateFoodDTO,
+  ) {
+    const editedFood = await this.foodService.editFood(foodID, createFoodDTO);
+    if (!editedFood) throw new NotFoundException('Food does not exist!');
+    return res.status(HttpStatus.OK).json({
+      message: 'Edit food success',
+      food: editedFood,
+    });
+  }
 
-    @Put('/edit')
-    async editFood(
-        @Res() res,
-        @Query('foodID', new ValidateObjectId()) foodID,
-        @Body() createFoodDTO: CreateFoodDTO
-    ) {
-        const editedFood = await this.foodService.editFood(foodID, createFoodDTO)
-        if(!editedFood) throw new NotFoundException('Food does not exist!')
-        return res.status(HttpStatus.OK).json({
-            message: 'Edit food success',
-            food: editedFood
-        })
-    }
-
-    @Delete('/delete')
-    async deleteFood(@Res() res, @Query('foodID', new ValidateObjectId()) foodID) {
-        const deletedFood = await this.foodService.deleteFood(foodID)
-        if(!deletedFood) throw new NotFoundException('Food does not exist!')
-        return res.status(HttpStatus.OK).json({
-            message: 'Deleted success',
-            food: deletedFood
-        })
-    }
+  @Delete('/delete')
+  async deleteFood(
+    @Res() res,
+    @Query('foodID', new ValidateObjectId()) foodID,
+  ) {
+    const deletedFood = await this.foodService.deleteFood(foodID);
+    if (!deletedFood) throw new NotFoundException('Food does not exist!');
+    return res.status(HttpStatus.OK).json({
+      message: 'Deleted success',
+      food: deletedFood,
+    });
+  }
 }
--- src/food/food.service.ts
+++ src/food/food.service.ts
@@ -1,40 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose'
-import { InjectModel } from '@nestjs/mongoose'
-import { Food } from './interfaces/food.interface'
-import { CreateFoodDTO } from './dto/create-food.dto'
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Food } from './interfaces/food.interface';
+import { CreateFoodDTO } from './dto/create-food.dto';
 
 @Injectable()
 export class FoodService {
+  constructor(@InjectModel('Food') private readonly foodModel: Model<Food>) {}
 
-    constructor(@InjectModel('Food') private readonly foodModel: Model<Food>) { }
+  async getFoods(): Promise<Food[]> {
+    const food = await this.foodModel.find().exec();
+    return food;
+  }
 
-    async getFood(): Promise<Food[]> {
-        const food = await this.foodModel.find().exec()
-        return food
-    }
+  async getFood(foodID): Promise<Food> {
+    const food = await this.foodModel.findById(foodID).exec();
+    return food;
+  }
 
-    async getFood(foodID): Promise<Food> {
-        const food = await this.foodModel
-            .findById(foodID)
-            .exec()
-        return food;
-    }
+  async addFood(createFoodDTO: CreateFoodDTO): Promise<Food> {
+    const newFood = await new this.foodModel(createFoodDTO);
+    return newFood.save();
+  }
 
-    async addFood(createFoodDTO: CreateFoodDTO): Promise<Food> {
-        const newFood = await this.foodModel(createFoodDTO);
-        return newFood.save()
-    }
-    
-    async editFood(foodID, createFoodDTO: CreateFoodDTO): Promise<Food> {
-        const editFood = await this.foodModel
-            .findByIdAndUpdate(foodID, createFoodDTO, {new: true})
-        return editFood
-    }
+  async editFood(foodID, createFoodDTO: CreateFoodDTO): Promise<Food> {
+    const editFood = await this.foodModel.findByIdAndUpdate(
+      foodID,
+      createFoodDTO,
+      { new: true },
+    );
+    return editFood;
+  }
 
-    async deleteFood(foodID): Promise<any> {
-        const deletedFood = await this.foodModel
-            .findByIdAndRemove(foodID)
-        return deletedFood
-    }
+  async deleteFood(foodID): Promise<any> {
+    const deletedFood = await this.foodModel.findByIdAndRemove(foodID);
+    return deletedFood;
+  }
 }
